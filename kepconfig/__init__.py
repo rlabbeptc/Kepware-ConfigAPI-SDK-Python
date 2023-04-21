7eae5cb2207e582abaa3e83aad3d331c6a69af66
Ray Labbe <61295936+rlabbeptc@users.noreply.github.com>
2023-04-21 15:58:07 -0400

    Test

--- kepconfig/__init__.py
+++ kepconfig/__init__.py
@@ -3,19 +3,23 @@
 # See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-r"""
+# r"""
 
-**Kepware Configuration (kepconfig) API Library**
---
+# **Kepware Configuration (kepconfig) API Library**
+# --
 
-This is a package to help create Python applications to conduct operations with the Kepware Configuration API. 
-This package is designed to work with all versions of Kepware that support the Configuration API including 
-Thingworx Kepware Server (TKS), Thingworx Kepware Edge (TKE) and KEPServerEX (KEP).
+# This is a package to help create Python applications to conduct operations with the Kepware Configuration API. 
+# This package is designed to work with all versions of Kepware that support the Configuration API including 
+# Thingworx Kepware Server (TKS), Thingworx Kepware Edge (TKE) and KEPServerEX (KEP).
 
-Copyright (c) PTC Inc. and/or all its affiliates. All rights reserved.
-See License.txt in the project root for
-license information.
+# Copyright (c) PTC Inc. and/or all its affiliates. All rights reserved.
+# See License.txt in the project root for
+# license information.
 
+# """
+
+"""
+.. include:: ../README.md
 """
 __version__ = "1.2.0"
 from . import connection, error
